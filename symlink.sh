08c617a7ad5438f0fae60f664610cc4196749c60
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2013-12-28 15:57:41 +0900

    add symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf $(cd $(dirname $0);pwd)/.bash_profile ~
+ln -sf $(cd $(dirname $0);pwd)/.bashrc ~
+ln -sf $(cd $(dirname $0);pwd)/.emacs ~
+ln -sf $(cd $(dirname $0);pwd)/.gitconfig ~
+ln -sf $(cd $(dirname $0);pwd)/.inputrc ~
+ln -sf $(cd $(dirname $0);pwd)/.vimrc ~
