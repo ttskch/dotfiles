5c8451937f1d30c2ca23cc3a2865634666e3aa95
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-01-05 16:46:32 +0900

    add .filezilla

--- symlink.sh
+++ symlink.sh
@@ -6,3 +6,4 @@ ln -sf $(cd $(dirname $0);pwd)/.emacs ~
 ln -sf $(cd $(dirname $0);pwd)/.gitconfig ~
 ln -sf $(cd $(dirname $0);pwd)/.inputrc ~
 ln -sf $(cd $(dirname $0);pwd)/.vimrc ~
+ln -sf $(cd $(dirname $0);pwd)/.filezilla ~
