9be0353116a8d4c40ba6217b81a205de9cbcf9cd
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-01-08 14:01:09 +0900

    add .tmux.conf

--- symlink.sh
+++ symlink.sh
@@ -7,3 +7,4 @@ ln -sf $(cd $(dirname $0);pwd)/.gitconfig ~
 ln -sf $(cd $(dirname $0);pwd)/.inputrc ~
 ln -sf $(cd $(dirname $0);pwd)/.vimrc ~
 ln -sf $(cd $(dirname $0);pwd)/.filezilla ~
+ln -sf $(cd $(dirname $0);pwd)/.tmux.conf ~
