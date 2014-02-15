7bb0beffca657e22cc275791d17960f1786261ed
Takashi Kanemoto <t-kanemoto@quartet-mail.com>
2014-02-15 17:40:26 +0900

    add .gitignore_global

--- symlink.sh
+++ symlink.sh
@@ -7,3 +7,4 @@ ln -sf $(cd $(dirname $0);pwd)/.vimrc ~
 ln -sf $(cd $(dirname $0);pwd)/.filezilla ~
 ln -sf $(cd $(dirname $0);pwd)/.tmux.conf ~
 ln -sf $(cd $(dirname $0);pwd)/.gemrc ~
+ln -sf $(cd $(dirname $0);pwd)/.gitignore_global ~
