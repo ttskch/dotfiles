2d93d952b27e819f4e9c1aed4f415eb75f1b3f18
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-10-04 01:43:39 +0900

    add docker config

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -16,3 +16,5 @@ export PATH
 
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+
+eval "$(docker-machine env default)"   # for docker
