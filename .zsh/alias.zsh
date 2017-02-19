e7865e73632df0e848a491d88538037b6f69f328
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-02-19 11:13:41 +0900

    update brew-cask-cleanup operation

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -28,6 +28,6 @@ alias hubb="hub browse"
 alias hubpr="hub pull-request"
 alias ydl="youtube-dl -x --audio-format=mp3"
 alias it="cd ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized"
-alias brew-cask-cleanup='for c in /opt/homebrew-cask/Caskroom/*; do vl=(`ls -t $c`) && for v in "${vl[@]:1}"; do grm -rf "$c/$v"; done; done'
+alias brew-cask-cleanup='for c in /usr/local/Caskroom/*; do vl=(`ls -t $c`) && for v in "${vl[@]:1}"; do grm -rf "$c/$v"; done; done'
 alias ql='qlmanage -p "$@" >& /dev/null'
 alias docker-ps='docker ps | peco | cut -d " " -f 1'
