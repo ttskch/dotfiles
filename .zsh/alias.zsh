a38a80728fecee878369bbf6d7c21adfa31bd100
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-02-27 12:53:46 +0900

    update alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -24,7 +24,7 @@ alias diff="git diff"
 alias hubb="hub browse"
 alias hubpr="hub pull-request"
 alias ydl="youtube-dl -x --audio-format=mp3"
-alias it="cd ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized"
+alias it='(){ mv $1 ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/ }'
 alias brew-cask-cleanup='for c in /usr/local/Caskroom/*; do vl=(`ls -t $c`) && for v in "${vl[@]:1}"; do grm -rf "$c/$v"; done; done'
 alias ql='qlmanage -p "$@" >& /dev/null'
 alias docker-ps='docker ps | peco | cut -d " " -f 1'
