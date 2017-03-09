12ebc4231df76f1e0a485f64051eee1725d11302
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-03-09 18:17:23 +0900

    remove unnecessary alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -28,5 +28,4 @@ alias ydl="youtube-dl -x --audio-format=mp3"
 alias it='(){ mv $1 ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/ }'
 alias brew-cask-cleanup='for c in /usr/local/Caskroom/*; do vl=(`ls -t $c`) && for v in "${vl[@]:1}"; do grm -rf "$c/$v"; done; done'
 alias ql='qlmanage -p "$@" >& /dev/null'
-alias docker-ps='docker ps | peco | cut -d " " -f 1'
 alias phpv="switch-php-version"
