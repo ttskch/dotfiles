a0140a5d999961de3f4e6dd91a26975d181b27a5
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2019-02-28 09:38:26 +0900

    install phpenv

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -8,14 +8,15 @@ export C_INCLUDE_PATH="/usr/include/"
 export GEM_HOME=$HOME/.gem
 
 PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:$PATH
-PATH=/usr/local/opt/php@7.1/bin:$PATH   # for php from brew.
-PATH=/usr/local/opt/httpd24/bin:$PATH   # for apache from brew.
-PATH=/usr/local/opt/node/bin:$PATH   # for node from brew.
-PATH=/usr/local/share/npm/bin:$PATH   # for npm apps.
-PATH=$HOME/.gem/ruby/2.4.0/bin:$PATH   # for gems.
-PATH=$HOME/.gem/bin:$PATH   # for gems.
-PATH=$HOME/.php/bin:$HOME/.php/sbin:$PATH   # for php from brew.
+PATH=/usr/local/opt/httpd24/bin:$PATH   # for apache from brew
+PATH=/usr/local/opt/node/bin:$PATH   # for node from brew
+PATH=/usr/local/share/npm/bin:$PATH   # for npm apps
+PATH=$HOME/.gem/ruby/2.4.0/bin:$PATH   # for gems
+PATH=$HOME/.gem/bin:$PATH   # for gems
+PATH=$HOME/.phpenv/bin:$PATH   # for phpenv
 export PATH
+
+eval "$(phpenv init -)"   # for phpenv
 
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
