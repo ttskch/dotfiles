b617a8938f42e1558f39b57ac7ffa0ebcf89cef0
Takashi Kanemoto <ttskch@gmail.com>
2020-11-20 11:37:48 +0900

    feat: 🎸 zsh: add alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -38,3 +38,4 @@ alias psy="psysh"
 alias sed="gsed"
 alias typora="open -a Typora"
 alias phpenv-install='PHP_BUILD_CONFIGURE_OPTS="--with-libedit=$(brew --prefix libedit)" phpenv install'
+alias sft="bin/phpunit"
