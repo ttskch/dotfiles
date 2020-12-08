26d9928566dd9ac16e78aa7c14a46dcbaf3e76d0
Takashi Kanemoto <ttskch@gmail.com>
2020-12-08 17:44:38 +0900

    feat: 🎸 zsh: tweak

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -8,7 +8,6 @@ export C_INCLUDE_PATH="/usr/include/"
 export GEM_HOME=$HOME/.gem
 
 PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:$PATH
-PATH=/usr/local/opt/httpd24/bin:$PATH # for apache from brew
 PATH=$HOME/bin:$PATH
 PATH=$HOME/.rbenv/bin:$PATH # for ruby from rbenv
 PATH=$HOME/.gem/bin:$PATH # for gems
--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 basepath=$(cd $(dirname $0);pwd)
 
