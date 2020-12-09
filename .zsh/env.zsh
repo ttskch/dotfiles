347678538279120595611a56bce3f62b04981cc9
Takashi Kanemoto <ttskch@gmail.com>
2020-12-09 10:58:31 +0900

    feat: 🎸 bin: tweak phpenv-install

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -12,7 +12,6 @@ PATH=$HOME/bin:$PATH
 PATH=$HOME/.rbenv/bin:$PATH # for ruby from rbenv
 PATH=$HOME/.gem/bin:$PATH # for gems
 PATH=$HOME/.phpenv/bin:$PATH # for phpenv
-PATH=/usr/local/opt/bison/bin:$PATH # for phpenv
 PATH=$HOME/.symfony/bin:$PATH # for symfony/cli
 PATH=$HOME/.nodebrew/current/bin:$PATH # for nodebrew
 PATH=$HOME/ghq/github.com/flutter/flutter/bin:$PATH   # for flutter
@@ -20,8 +19,6 @@ export PATH
 
 eval "$(phpenv init -)"   # for phpenv
 eval "$(rbenv init -)"   # for rbenv
-
-export PKG_CONFIG_PATH=/usr/local/opt/krb5/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/icu4c/lib/pkgconfig:/usr/local/opt/libedit/lib/pkgconfig:/usr/local/opt/libxml2/lib/pkgconfig   # for phpenv
 
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
