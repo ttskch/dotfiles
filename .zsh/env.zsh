f49cf1ef241d1e163fbe5b7f593ae63610c3f9db
Takashi Kanemoto <ttskch@gmail.com>
2020-12-20 16:38:29 +0900

    feat: 🎸 zsh: use z

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -14,11 +14,13 @@ PATH=$HOME/.gem/bin:$PATH # for gems
 PATH=$HOME/.phpenv/bin:$PATH # for phpenv
 PATH=$HOME/.symfony/bin:$PATH # for symfony/cli
 PATH=$HOME/.nodebrew/current/bin:$PATH # for nodebrew
-PATH=$HOME/ghq/github.com/flutter/flutter/bin:$PATH   # for flutter
+PATH=$HOME/ghq/github.com/flutter/flutter/bin:$PATH # for flutter
 export PATH
 
-eval "$(phpenv init -)"   # for phpenv
-eval "$(rbenv init -)"   # for rbenv
+eval "$(phpenv init -)" # for phpenv
+eval "$(rbenv init -)" # for rbenv
+
+. $(brew --prefix z)/etc/profile.d/z.sh # for z
 
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
