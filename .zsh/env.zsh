de0033a51612ba9cb6cfb54354877e0e9333a7d2
Takashi Kanemoto <ttskch@gmail.com>
2021-07-27 15:40:00 +0900

    feat: 🎸 tweak

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -25,3 +25,5 @@ eval "$(direnv hook zsh)" # for direnv
 
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+
+ulimit -n 1024 # avoid "Too many open files"
