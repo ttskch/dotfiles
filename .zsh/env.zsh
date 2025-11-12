044dfd2d107c1ea00effa939680907634dc7a179
Takashi Kanemoto <ttskch@gmail.com>
2025-11-12 11:20:48 +0900

    feat: 🎸 task completion

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -37,3 +37,6 @@ export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46
 zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
 
 ulimit -n 1024 # avoid "Too many open files"
+
+# Task
+eval "$(task --completion zsh)"
